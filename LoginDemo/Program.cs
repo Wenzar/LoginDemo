a632a7ba60021d30001e4daab9f820edcbdef6bc
nesmachnyi <m.nesmachnyi@itelma.su>
2025-11-11 12:09:06 +0300

    .

--- LoginDemo/Program.cs
+++ LoginDemo/Program.cs
@@ -1,25 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
